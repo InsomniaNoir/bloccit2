378c94e05a02ae7876b846cef79cd4e5b7c923ee
Jeremiah Shafer <jrshafer@mac.com>
2015-06-17 15:04:36 -0500

    Update post_policy

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,4 +1,25 @@
 class PostPolicy < ApplicationPolicy
+  class Scope < Scope
+    attr_reader :user, :scope
+
+    def initializer(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.admin?
+        scope.all?
+      else
+        scope.where(:published => true)
+      end
+    end
+  end
+
+  def update?
+    user.admin? or not post.published?
+  end
+
   def index?
     true
   end
