adffb40be497b892df6182081092a28d2c68c793
Jeremiah Shafer <jrshafer@mac.com>
2015-06-23 20:18:15 +0000

    Post markdown assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_one :summary
   belongs_to :topic
+  
+  
 
   default_scope { order('created_at DESC') }
 
@@ -10,4 +12,20 @@ class Post < ActiveRecord::Base
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
+  
+  def markdown_title
+    render_as_markdown title
+  end
+  
+  def markdown_body
+    render_as_markdown body
+  end
+  
+private
+  def render_as_markdown
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render markdown).html_safe
+  end
 end
