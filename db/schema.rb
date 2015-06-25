c8482eb7a1cadb00916ba90cff3ca13df126656d
Jeremiah Shafer <jrshafer@mac.com>
2015-06-24 19:34:52 -0500

    Debug fog error rails server

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150620002928) do
+ActiveRecord::Schema.define(version: 20150624004401) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20150620002928) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
