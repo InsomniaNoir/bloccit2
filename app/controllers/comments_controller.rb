ec1fe8bff613ad0d2eec815ab7f7cca88058c13d
Jeremiah Shafer <jrshafer@mac.com>
2015-07-04 11:53:49 -0500

    Please work

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
-    @comment.user = current_user
-
+    @comment.user_id = current_user.id
     if @comment.save
       redirect_to [@post.topic, @post], notice: "Comment saved successfully."
     else
@@ -11,19 +10,6 @@ class CommentsController < ApplicationController
     end
    end
 
-  def destroy
-    @comment = Comment.find(params[:id])
-    @post = @comment.post
-
-    authorize @comment
-    if @comment.destroy
-      flash[:notice] = "Comment was removed."
-      redirect_to @post
-    else
-      flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to @post
-    end
-  end
 
 private
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,9 @@
 class PostsController < ApplicationController
    def show
+     @topic = Topic.find(params[:topic_id])
      @post = Post.find(params[:id])
      @comments = @post.comments
-     @comment = Comment.new
+     @comment = @post.comments.build
    end
 
    def new
