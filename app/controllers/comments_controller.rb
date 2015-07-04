e4b5dc54df65117583d29771af0e5a7e1c48241c
Jeremiah Shafer <jrshafer@mac.com>
2015-07-04 12:06:51 -0500

    Debug destroy-assignment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,14 +2,27 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
-    @comment.user_id = current_user.id
+    @comment.user = current_user
     if @comment.save
-      redirect_to [@post.topic, @post], notice: "Comment saved successfully."
+      redirect_to @post, notice: "Comment saved successfully."
     else
-      redirect_to [@post.topic, @post], notice: "Comment failed to save."
+      redirect_to @post, notice: "Comment failed to save."
     end
    end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @post = @comment.post
+
+    authorize @comment
+    if @comment.destroy
+      flash[:notice] = "Comment was deleted"
+      redirect_to @post
+    else
+      flash[:error] = "Comment couldn't be deleted. Please try again."
+      redirect_to @post
+    end
+  end
 
 private
 
