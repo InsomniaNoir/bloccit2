48342c5f947abc624ff2b73af01e3853f74baadb
Jeremiah Shafer <jrshafer@mac.com>
2015-07-13 20:30:04 -0500

    rest

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+RSpec.describe Vote do
+  vote = Vote.new
+  describe "validations" do
+    describe "value validations" do
+      it "allows -1 or 1 as values"
+      expect(vote).to_eq( 1, -1 )
+    end
+  end
+end
