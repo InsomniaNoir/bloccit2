436989ca1307688bd00b3e5452b927a4b1de0a1d
Jeremiah Shafer <jrshafer@mac.com>
2015-07-25 13:35:38 -0500

    Relocate test factories. VoteSpec passes

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -18,10 +18,22 @@ require 'rails_helper'
 
    describe 'after_save' do
      it "calls `Post#update_rank` after save" do
-       post = associated_post
-       vote = Vote.new(value: 1, post: post)
-       expect(post).to receive(:update_rank)
+       @post = associated_post
+       vote = Vote.new(value: 1, post: @post)
+       expect(@post).to receive(:update_rank)
        vote.save
      end
    end
+
+   def associated_post(options = {})
+     post_options = {
+       title: 'Post title',
+       body: 'Post bodies must be pretty long',
+       topic: Topic.create(name: 'Topic name'),
+       user: @user
+     }.merge(options)
+
+     Post.create(post_options)
+   end
+
  end
