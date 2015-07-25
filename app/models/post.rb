3eddd1c825c0102835c08dba2392cb8a64441391
Jeremiah Shafer <jrshafer@mac.com>
2015-07-24 20:48:40 -0500

    Finish Voting Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -28,8 +28,16 @@ class Post < ActiveRecord::Base
     update_attribute(:rank, new_rank)
   end
 
+  after_create :create_vote
+
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
+
+  private
+
+  def create_vote
+    self.user.votes.create(value: 1)
+  end
 end
