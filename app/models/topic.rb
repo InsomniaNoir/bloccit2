d8763336cb2de1becb179d7924b508cfb5c77b23
Jeremiah Shafer <jrshafer@mac.com>
2015-07-26 19:30:03 -0500

    Add policy info to Topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,5 +3,5 @@ class Topic < ActiveRecord::Base
 
   validates :name, length: { minimum: 5 }, presence: true
 
-  self.per_page = 50
+  scope :visible_to, -> (user) { user ? all : where(public: true) }
 end
