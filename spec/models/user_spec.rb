f6343359d0f6fae56969bddf0dcf01d218433d67
Jeremiah Shafer <jrshafer@mac.com>
2015-07-27 20:32:49 -0500

    RSpec test for favorites passed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,11 +4,20 @@ describe User do
 
   include TestFactories
 
+  before do
+    @post = associated_post
+    @user = authenticated_user
+  end
+
   describe "#favorited(post)" do
     it "returns 'nil' if the user has not favorited the post" do
+      expect(@user.favorited(@post)).to be_nil
     end
 
     it "returns the appropriate favorite if it exists" do
+      @user.favorites.create(post: @post)
+      favorite = @user.favorites.find_by_post_id(@post.id)
+      expect(@user.favorited(@post)).to eq(favorite)
     end
   end
 end
